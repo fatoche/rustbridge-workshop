9feaa6d3740e52e8f5f58688f3f217fa4c25a01f
Clara Scherer <git20357@s.cherer.de>
2018-05-27 11:37:38 +0200

    fix the compiler error with exactly the type we were told

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     // this is a comment
     println!("Hello, RustBridge Paris!");
 
-    let name: String = "Fuchur";
+    let name: &'static str = "Fuchur";
     let age = 7;
     println!("{} is {} years old.", name, age);
 }
